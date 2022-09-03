25b6e388addfd1b783e2343f496671cbc7bb828d
Priyanshu360-cpu <tpriyanshu775@gmail.com>
2022-09-04 02:25:05 +0530

    vector temp file added

---
+++ vector.js
@@ -0,0 +1,30 @@
+class vector{
+    data=[];
+    constructor(){
+    }
+    push_back(data){
+        this.data.push(data);
+    }
+    pop_back(){
+        if(this.data.length>0)
+            this.data.pop();
+        else throw "Empty Vector";
+    }
+    print(){
+        for(let i=0;i<this.data.length;i++){
+            console.log(this.data[i]);
+        }
+    }
+    sort(){
+        this.data.sort();
+    }
+    size(){
+        return this.data.length;
+    }
+    at(index){
+        return this.data[index];
+    }
+    erase(){
+        this.data=[];
+    }
+}
