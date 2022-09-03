40cc05f5d5926352fda9c559dc031d7e7effcf3c
Priyanshu360-cpu <tpriyanshu775@gmail.com>
2022-09-04 02:09:09 +0530

    added print and depth

--- binarytree.js
+++ binarytree.js
@@ -7,5 +7,27 @@ class binarytree{
         this.left=null;
         this.right=null;
     }
+    print(){
+        console.log(this.data);
+        temp=this.left;
+        while(temp!=null){
+            temp.print();
+            temp=temp.right;
+        }
+    }
+    depth(){
+        if(this.left==null && this.right==null){
+            return 0;
+        }
+        else if(this.left==null){
+            return this.right.depth()+1;
+        }
+        else if(this.right==null){
+            return this.left.depth()+1;
+        }
+        else{
+            return Math.max(this.left.depth(),this.right.depth())+1;
+        }
+    }   
 }
 module.exports=binarytree;
