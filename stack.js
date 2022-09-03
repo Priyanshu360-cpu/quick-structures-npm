f548e583c6ac7b850dc8f39f7309a15bfc2df496
Priyanshu360-cpu <tpriyanshu775@gmail.com>
2022-09-04 02:15:16 +0530

    added stack temp file

---
+++ stack.js
@@ -0,0 +1,24 @@
+class stack{
+    data;
+    next;
+    constructor(data){
+        this.data=data;
+        this.next=null;
+    }
+    push(data){
+        let temp=new stack(data);
+        temp.next=this.next;
+        this.next=temp;
+    }
+    pop(){
+        this.next=this.next.next;
+    }
+    print(){
+        let temp=this;
+        while(temp!=null){
+            console.log(temp.data);
+            temp=temp.next;
+        }
+    }
+}
+module.exports=stack;
