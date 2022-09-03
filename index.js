1c02f68f95afb1648ccdc0f02a30088852ecbba1
Priyanshu360-cpu <tpriyanshu775@gmail.com>
2022-09-04 02:19:42 +0530

    removed missing files

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ class structures{
     constructor(){
     this.linkedlist = require('./linkedlist.js');
     this.stack = require('./stack.js');
-    this.queue = require('./queue.js');
     this.tree = require('./binarytree.js');
     this.graph = require('./graph.js'); 
  }
